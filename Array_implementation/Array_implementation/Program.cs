217f9b56142690ff8a88fffddc8cab4f556c848a
Larrykatuva <larry.katuva@gmail.com>
2022-06-12 08:29:10 +0300

    array operations

--- Array_implementation/Array_implementation/Program.cs
+++ Array_implementation/Array_implementation/Program.cs
@@ -25,10 +25,12 @@ public class CustomArrayList<T>
     ///<param name ="item">The element you want to add</param>
     public void Add(T item)
     {
-        GrowIfIsFull();
+        GrowIfArrIsFull();
         this.arr[this.count]=item;
         this.count++;
     }
+
+
     ///<summary>Inserts the specified element at given position in this list</summary>
     /// <param name="index">
     /// index , at which the specified element is to be inserted</param>
@@ -39,10 +41,74 @@ public class CustomArrayList<T>
         if(index > this.count || index< 0)
         {
             throw new IndexOutOfRangeException("Invalid index" +index);
+        }
+        GrowIfArrIsFull();
+        Array.Copy(this.arr,   index,this.arr,index + 1, this.count - index);
+        this.arr[index] = item;
+        this.count++;
+    }
+    ///<summary>
+    ///Double the size of this array if its full
+    ///</summary>
+    private void GrowIfArrIsFull()
+    {
+        if(this.count + 1 > this.arr.Length)
+        {
+            T[] extentedArr = new T[this.arr.Length * 2];
+            Array.Copy(this.arr, extentedArr, this.count);
+            this.arr = extentedArr;
+        }
 
+    }
+    ///<summary>Clear the list </summary>
+    public void Clear()
+    {
+        this.arr = new T[INITIAL_CAPACITY];
+        this.count = 0;
+    }
+    ///summary>
+    ///return the index of the first occurrence of the specified 
+    ///element in this list (or -1 if it does not exist ).
+    ///</summary>
+    ///<typeparam name="item">the element you are serching</param>
+    ///<returns>
+    ///the index of a given element or -1     if not   found
+    ///</returns>
+    public int IndexOf(T item)
+    {
+        for (int i = 0; i < this.arr.Length; i++)
+        {
+            if (object.Equals(item, this.arr[i]))
+            {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    ///<summary>Checks if an element exists </summary>
+    ///<param name="item">The item to be checked param>
+    ///<returns> If thre item exist</returns>
+    public bool Contains(T item)
+    {
+        int index = IndexOf(item);
+        bool found = (index != -1);
+        return found;
+    }
+    ///<summary>Indexer:access to element at a given position </summary>
+    ///<param name="index">Index of the element</param>
+    ///<returns> the element the specified position</returns>
+    public T this[int index]
+    {
+        get
+        {
+            if(index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("invalid index" + index);
+            }
+            return this.arr[index];
         }
     }
-
 
 
 }
